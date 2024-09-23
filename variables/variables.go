e6f19ebf83dbedde466e3583f3249e01ce6f4c4c
firoj-siddique <firoj.siddique@neuron7.ai>
2024-09-23 09:42:34 +0530

    Added simple var and float

---
+++ variables/variables.go
@@ -0,0 +1,28 @@
+package main
+import "fmt"
+
+func main(){
+	fmt.Println("Hi")
+	a := 5
+	b := 7
+	var c float64  = 9.2
+	var d float64  = 11.1
+	fmt.Printf("The sum of %d and %d is: %d\n", a,b,a+b)
+	fmt.Printf("The diff of %d and %d is: %d\n", a,b,a-b)
+	fmt.Printf("The div of %d and %d is: %d\n", a,b,a/b)
+	fmt.Printf("The actual div of %d and %d is: %f\n", a,b, float64(a)/float64(b))
+	fmt.Printf("The multiply of %d and %d is: %d\n", a,b,a*b)
+
+	fmt.Println("-----------------------------------------")
+	fmt.Printf("The sum of %f and %f is: %f\n", c,d,c+d)
+	fmt.Printf("The diff of %f and %f is: %.2f\n", c,d,c-d)
+	fmt.Printf("The multiply of %f and %f is: %.2f\n", c,d,c*d)
+	fmt.Printf("The div of %f and %f is: %.2f\n", c,d,c/d)
+
+	fmt.Println("-----------------------------------------")
+	var e = "My name is"
+	var f = "Firoz"
+
+	fmt.Printf(e+" "+f)
+
+}
