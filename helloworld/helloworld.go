8409367b99ff1745d69062823532bfea84cce962
firoj-siddique <firoj.siddique@neuron7.ai>
2024-09-23 09:20:31 +0530

    Added different print format

--- helloworld/helloworld.go
+++ helloworld/helloworld.go
@@ -4,5 +4,9 @@ import "fmt";
 func main() {
 
 	fmt.Println("Hello World!!!")
+	fmt.Println("----------")
+	fmt.Printf("Hello %s, you are %d years old.\n", "Alice", 30)
+	fmt.Println("----------")
+	message := fmt.Sprintf("Hello %s, you are %d years old.", "Alice", 30)
+	fmt.Println(message)
 }
-
